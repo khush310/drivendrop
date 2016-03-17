59d0bcb619f45ab86ce971ccfe4d95d86565f58b
khushboo <khush310@gmail.com>
2016-03-17 10:05:52 +0100

    css edit profile

--- src/components/editprofile.js
+++ src/components/editprofile.js
@@ -7,29 +7,29 @@ class EditProfile extends React.Component {
 		const user = store.cursor(["profileUser"]).deref();
 		return (
 			<div className="stage profile" style={{paddingTop: "1em", color: "rgb(111, 110, 110)"}}>
-				<div className="otherinfo">
-					Name
+				<div className="otherinfo editinfo">
+					<span>Name</span>
 					<input type="text" placeholder={user.get("name")}/>
 				</div>
-				<div className="otherinfo">
-					Surname
+				<div className="otherinfo editinfo">
+					<span>Surname</span>
 					<input type="text" placeholder={user.get("surname")}/>
 				</div>
-				<div className="otherinfo">
-					Sex
+				<div className="otherinfo editinfo">
+					<span>Sex</span>
 					<input type="text" placeholder={user.get("gender")}/>
 				</div>
-				<div className="otherinfo">
-					Why we should travel together
-					<input type="text" placeholder={user.get("about")}/>
-				</div>
-				<div className="otherinfo">
-					Email
+				<div className="otherinfo editinfo">
+					<span>Email</span>
 					<input type="text" placeholder={user.get("email")}/>
 				</div>
-				<div className="otherinfo">
-					Phone Number
+				<div className="otherinfo editinfo">
+					<span>Phone No.</span>
 					<input type="text" placeholder={user.get("phone")}/>
+				</div>
+				<div className="otherinfo editinfo">
+					Why we should travel together
+					<input type="text" placeholder={user.get("about")}/>
 				</div>
 			</div>
 		)
