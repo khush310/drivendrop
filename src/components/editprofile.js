88ba60ee17ef4348257e479ee2817160e2be23a3
khushboo <khush310@gmail.com>
2016-03-21 22:06:34 +0100

    edit profile

--- src/components/editprofile.js
+++ src/components/editprofile.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from 'react-router';
-import {center} from "../stylesheets/center";
+import {buttonStyle, buttonLink} from "../stylesheets/button";
 class EditProfile extends React.Component {
 	constructor(props) {
 		super(props);
@@ -44,43 +44,47 @@ class EditProfile extends React.Component {
 	render(){
 		const {store} = this.props;
 		const user = store.cursor(["profileUser"]).deref();
+		const nameStyle = {display:"flex", width:"90%", borderBottom:"1px solid rgba(66, 66, 66, 0.5)", justifyContent:"space-around", alignItems:"center", height:"40px"};
+		const inputStyle = {width:"60%", borderBottom:"none"};
+		const button=Object.assign({},buttonStyle,{height:"7%", width: "80%", margin: "1.5em 0", background: "#00c5d1", borderRadius: "40px", border:"#00c5d1"});
+		const linkStyle = Object.assign({}, buttonLink, {color: "white", fontSize: "5vw"});
+
 		return (
 			<div className="stage editprofile" style={{paddingTop: "1em", color: "rgb(111, 110, 110)", overflow:"scroll"}}>
-				<div className="heading">About</div>
-				<div style={{height: "15%", width: "100%", borderBottom: "1px solid rgba(61, 61, 61, 0.2)"}}>
-					<i className="fa fa-quote-left fa-2x fa-pull-left fa-border"></i>
-					<input style={{width: "85%", fontSize: "1em", height:"100%", }} type="text" onChange={this.saveAbout} placeholder={user.get("about")}/>
+				<div style={{fontWeight:"bold", fontSize:"1.2em"}}>About</div>
+				<div style={{height: "15%", width: "100%"}}>
+					<i style={{margin:"0"}} className="fa fa-quote-left fa-2x fa-pull-left fa-border"></i>
+					<input style={{width: "80%", fontSize: "1em", height:"100%", borderBottom:"none"}} type="text" onChange={this.saveAbout} placeholder={user.get("about")}/>
 				</div>
-
-				<div className="heading">Name</div>
-				<div>
-					<div className="otherinfo editinfo">
+				<div style={{fontWeight:"bold", fontSize:"1.2em"}}>Name</div>
+				<div style={{width:"100%", display:"flex", flexDirection:"column", alignItems:"center", margin:"1em"}}>
+					<div style={nameStyle}>
 						<span>First Name</span>
-						<input type="text" onChange={this.saveName} placeholder={user.get("name")}/>
+						<input type="text" style={inputStyle} onChange={this.saveName} placeholder={user.get("name")}/>
 					</div>
-					<div className="otherinfo editinfo">
+					<div style={nameStyle}>
 						<span>Last Name</span>
-						<input type="text" onChange={this.saveSurname} placeholder={user.get("surname")}/>
+						<input type="text" style={inputStyle} onChange={this.saveSurname} placeholder={user.get("surname")}/>
 					</div>
 				</div>
 
 				<div className="heading">Private Details</div>
-				<div>
-					<div className="otherinfo editinfo">
+				<div style={{width:"100%", display:"flex", flexDirection:"column", alignItems:"center", margin:"1em"}}>
+					<div style={nameStyle}>
 						<span>Sex</span>
-						<input type="text" onChange={this.saveGender} placeholder={user.get("gender")}/>
+						<input type="text" style={inputStyle} onChange={this.saveGender} placeholder={user.get("gender")}/>
 					</div>
-					<div className="otherinfo editinfo">
+					<div style={nameStyle}>
 						<span>Email</span>
-						<input type="text" onChange={this.saveEmail} placeholder={user.get("email")}/>
+						<input type="text" style={inputStyle} onChange={this.saveEmail} placeholder={user.get("email")}/>
 					</div>
-					<div className="otherinfo editinfo">
+					<div style={nameStyle}>
 						<span>Phone No.</span>
-						<input type="text" onChange={this.savePhone} placeholder={user.get("phone")}/>
+						<input type="text" style={{width:"65%", borderBottom:"none"}} onChange={this.savePhone} placeholder={user.get("phone")}/>
 					</div>
 				</div>
-				<div  style={{border:"1px solid red"}}>
-					<Link to="/public/profile/:username">Save</Link>
+				<div className="dndbutton" style={button}>
+					<Link style={linkStyle} to="/public/profile/:username">Save</Link>
 				</div>
 			</div>
 		)
